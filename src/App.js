925f88f6893fe843e64d4da7ed4075e0ad56083b
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-09-07 12:42:31 +0330

    first commit in admin dashboard branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import SideBarCom from './component/Sidebar';
+import SideBarCom from './components/Sidebar';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 export default function App() {
