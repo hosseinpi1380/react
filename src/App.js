80174a90f084f45cf23e8fbe7f96016ba3c0f335
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-09-13 15:17:16 +0330

    useMemo hook 01

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   const [newNotes, setNewNotes] = useState('')
   const [theme,setTheme]=useState('light');
   useEffect(()=>{
-    if(theme=='dark'){
+    if(theme==='dark'){
       document.documentElement.classList.add('dark')
     }else{
       document.documentElement.classList.remove('dark')
@@ -27,7 +27,7 @@ export default function App() {
  }
   return (
     <>
-      <div className='shadow-lg p-4 dark:bg-slate-800 dark:text-white rounded-md'>
+      <div className='shadow-lg p-4 dark:bg-slate-800 dark:text-white rounded-md max-sm:text-sm max-sm:font-normal max-sm:h-4/5'>
         <button onClick={hadnleThemeSwith} className={theme === 'dark' ? '' : 'hidden'}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
