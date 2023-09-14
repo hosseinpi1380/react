97a44d3ea840287889639a8248c3c8911c06d542
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-09-14 14:22:23 +0330

    delete code from prev branch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './index.css';
-import FilterNotes from './FilterNote';
+// import FilterNotes from './FilterNote';
 export default function App() {
-  const [notes, setNotes] = useState([]);
-  const [newNotes, setNewNotes] = useState('')
-  const [theme, setTheme] = useState('light');
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-  }, [theme])
-  const addHandler = () => {
-    let obj = {
-      id: notes.length + 1,
-      title: newNotes.toLowerCase()
-    }
-    setNotes(prev => [...prev, obj])
-    setNewNotes('')
-  }
-  const hadnleThemeSwith = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark')
-  }
-
+ 
   return (
     <>
-      
+      <input type="text" />
     </>
   )
 }
