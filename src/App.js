710216a456dc8e7234cccf411e37354c5c7a77fa
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-09-14 17:11:26 +0330

    useCallBakc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useCallback, memo, useMemo } from 'react';
 import './index.css';
-// import FilterNotes from './FilterNote';
 export default function App() {
- 
+  const [age, setAge] = useState(25);
+  const [salary, setSalary] = useState(5000)
+
+  const ageHandler = useCallback(() => {
+    console.log('age increacment');
+    setAge(age + 1)
+  },[age])
+
+  const salaryHandler = useCallback(() => {
+    console.log('salary increament');
+    setSalary(salary - 1)
+  },[salary])
   return (
     <>
-      <input type="text" />
+      <TitleComponent />
+      <Count text='age' count={age} />
+      <Button handler={ageHandler} text='age' />
+      <Count text='salary' count={salary} />
+      <Button handler={salaryHandler} text='salary' />
     </>
   )
 }
-
+const Button = memo(({ handler, text }) => {
+  console.log('rendering button',handler);
+  return (
+    <>
+      <button onClick={handler} className={`btn ${text === 'age' ? 'add' : 'min'}`}>
+        {text}</button>
+    </>
+  )
+})
+const TitleComponent = memo(() => {
+  console.log('title component');
+  return (
+    <>
+      <h1>title component</h1>
+    </>
+  )
+})
+const Count = memo(({ text, count }) => {
+  console.log('count component did update');
+  return (
+    <h1>{text} -{count}</h1>
+  )
+})
