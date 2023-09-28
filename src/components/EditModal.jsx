34f53cb5dc11d9e7fc5cfadd50839cb11866e584
Hossein Pirasbaghi <hosseinpirasbaghi@gmail.com>
2023-09-28 23:17:27 +0330

    cms project

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EditModal = ({ onClose, onSubmit, detailProduct }) => {
+    const [updatedObj, setUpdatedObj] = useState();
+    const editRequest=()=>{
+        fetch(`http://localhost:3000/api/products/${detailProduct.id}`,{
+            method:"PUT",
+            body:JSON.stringify(updatedObj)
+        })
+    }
     return (
         <div className='main-modal modal-active'>
             <div className='backdrop-blur-sm w-screen h-screen'>
@@ -8,11 +15,12 @@ const EditModal = ({ onClose, onSubmit, detailProduct }) => {
             <div className='modal w-1/2 h-1/2 text-right'>
                 <form onSubmit={onSubmit} className='flex flex-col text-right'>
                     <h1>اطلاعات جدید را وارد نمایید</h1>
-                    <input type="text" placeholder='اسم' value={detailProduct.title} />
-                    <input type="text" placeholder='قیمت' value={new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(detailProduct.price)} />
-                    <input type="text" placeholder='موجودی' value={detailProduct.count} />
-                    <input type="submit" className='bg-gray-500 text-white w-full mt-4 px-4 py-2' value='ثبت اطلاعات جدید' />
-                    <button onClick={onClose} className='bg-red-500 px-4 py-2 w-full mt-4 uppercase'>close</button>
+                    <input type="text" placeholder={detailProduct.title} defaultValue={detailProduct.title}/>
+                    <input type="text" placeholder={Number(detailProduct.price).toLocaleString()} />
+                    <input type="text" placeholder={detailProduct.count} />
+                    <input type="submit" className='bg-blue-700 text-white w-full mt-4 px-4 py-2' 
+                    value='ثبت اطلاعات جدید' onClick={editRequest}/>
+                    <button onClick={onClose} className='bg-red-700 px-4 py-2 w-full text-white mt-4 uppercase'>بستن</button>
                 </form>
             </div>
         </div>
